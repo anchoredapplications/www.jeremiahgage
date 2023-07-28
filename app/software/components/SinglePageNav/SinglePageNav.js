add0fd7472090e403c5ed5fa014d22fed69188ff
Jeremiah Gage <jgage6@oreillyauto.com>
2023-07-28 16:09:27 -0500

    Bug fixes

--- app/software/components/SinglePageNav/SinglePageNav.js
+++ app/software/components/SinglePageNav/SinglePageNav.js
@@ -3,30 +3,30 @@ import styles from './SinglePageNav.module.scss'
  
 function SinglePageNav(props) {
     useEffect(() => {
-        var sections = document.querySelectorAll("section");
-        function alignFrontAndBackOfCards() {
-            const sectionArr = []
-            sections.forEach(section => {
-                section.setAttribute("style", ``)
-                sectionArr.push(section)
-            })
-            sectionArr.shift();
-            sections.forEach(section => {
-                let heights = []
-                section.querySelectorAll("*").forEach(descandant => {
-                    heights.push(descandant.offsetHeight? Number(descandant.offsetHeight) : 0)
-                })
-                heights.push(section.offsetHeight? Number(section.offsetHeight) : 0)
-                const maxHeight = Math.max( ...heights );
-                section.setAttribute("style", `height: ${maxHeight}px;`)
-            })
-        }
-        window.addEventListener('resize', () => {
-            alignFrontAndBackOfCards();
-        }, true);
-        document.addEventListener("data-loaded", function(e) {
-            alignFrontAndBackOfCards();
-        });
+        //var sections = document.querySelectorAll("section");
+        // function alignFrontAndBackOfCards() {
+        //     const sectionArr = []
+        //     sections.forEach(section => {
+        //         section.setAttribute("style", ``)
+        //         sectionArr.push(section)
+        //     })
+        //     sectionArr.shift();
+        //     sections.forEach(section => {
+        //         let heights = []
+        //         section.querySelectorAll("*").forEach(descandant => {
+        //             heights.push(descandant.offsetHeight? Number(descandant.offsetHeight) : 0)
+        //         })
+        //         heights.push(section.offsetHeight? Number(section.offsetHeight) : 0)
+        //         const maxHeight = Math.max( ...heights );
+        //         section.setAttribute("style", `height: ${maxHeight}px;`)
+        //     })
+        // }
+        // window.addEventListener('resize', () => {
+        //     alignFrontAndBackOfCards();
+        // }, true);
+        // document.addEventListener("data-loaded", function(e) {
+        //     alignFrontAndBackOfCards();
+        // });
     }, [])
       
     return (
