225b7310bc380bfcbbac9a57fd4dbbc08eb9ae8a
Jeremiah Gage <jgage6@oreillyauto.com>
2023-07-28 15:56:44 -0500

    Bug fixes

--- app/page.js
+++ app/page.js
@@ -5,10 +5,6 @@ import Section from './software/App';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 require('dotenv').config()
-if (!document) {
-    const document = ""
-}
-
 
 export default function Home() {
   return (
