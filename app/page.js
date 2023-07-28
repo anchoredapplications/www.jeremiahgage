3aabdf929abd5e3be09f8400bd8a0e25cc8fadd6
Jeremiah Gage <jgage6@oreillyauto.com>
2023-07-28 16:22:13 -0500

    Bug fixes

--- app/page.js
+++ app/page.js
@@ -2,26 +2,13 @@
 import React from 'react';
 import './global.css';
 import Section from './software/App';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 require('dotenv').config()
 
 export default function Home() {
   return (
     <main>
-        <BrowserRouter>
-            <Routes>
-                <Route 
-                    path="/" 
-                    element={<Section/>}   
-                />
-                <Route 
-                    path="/auth" 
-                    element={"auth"}   
-                />           
-                <Route path="*" element={<Section/>} />
-            </Routes>
-        </BrowserRouter>
+        <Section />
     </main>
   )
 }
